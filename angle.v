1731ccf581f7a6e8adbfa524b690ebabdd4e00c0
Wobblyyyy <wobblyyyy@gmail.com>
2022-05-09 14:05:15 -0400

    add some short comments

--- angle.v
+++ angle.v
@@ -20,6 +20,7 @@ pub:
 	rad f64
 }
 
+// new_angle create a new angle
 pub fn new_angle(deg f64, rad f64) Angle {
 	return Angle{
 		deg: deg
@@ -27,18 +28,23 @@ pub fn new_angle(deg f64, rad f64) Angle {
 	}
 }
 
+// from_deg create a new angle based on that angle's degrees value
 pub fn from_deg(deg f64) Angle {
 	return new_angle(deg, math.radians(deg))
 }
 
+// from_rad create a new angle based on the angle's radians value
 pub fn from_rad(rad f64) Angle {
 	return new_angle(math.degrees(rad), rad)
 }
 
+// atan2 create a new angle using math.atan2
 pub fn atan2(y f64, x f64) Angle {
 	return from_rad(math.atan2(y, x))
 }
 
+// fix_deg make sure a degree value is greater than or equal to 0 and less
+// than or equal to 360
 pub fn fix_deg(deg f64) f64 {
 	mut ret := deg
 
@@ -53,6 +59,8 @@ pub fn fix_deg(deg f64) f64 {
 	return ret
 }
 
+// fix_rad make sure a radian value is greater than or equal to 0 and less
+// than or equal to 2 * pi
 pub fn fix_rad(rad f64) f64 {
 	mut ret := rad
 
@@ -67,42 +75,52 @@ pub fn fix_rad(rad f64) f64 {
 	return ret
 }
 
+// fixed_deg create a new angle by fixing a provided degree value
 pub fn fixed_deg(deg f64) Angle {
 	return from_deg(fix_deg(deg))
 }
 
+// fixed_rad create a new angle by fixing a provided radian value
 pub fn fixed_rad(rad f64) Angle {
 	return from_rad(fix_rad(rad))
 }
 
+// add add two angles together and fix the result
 pub fn add(a Angle, b Angle) Angle {
 	return fixed_deg(a.deg + b.deg)
 }
 
+// subtract angle b from angle a and fix the result
 pub fn subtract(a Angle, b Angle) Angle {
 	return fixed_deg(b.deg - a.deg)
 }
 
+// fix fix an angle
 pub fn (a Angle) fix() Angle {
 	return fixed_deg(a.deg)
 }
 
+// sin get the sine of this angle
 pub fn (a Angle) sin() f64 {
 	return math.sin(a.rad)
 }
 
+// cos get the cosine of this angle
 pub fn (a Angle) cos() f64 {
 	return math.cos(a.rad)
 }
 
+// tan get the tangent of this angle
 pub fn (a Angle) tan() f64 {
 	return math.tan(a.rad)
 }
 
+// add add two angles together
 pub fn (a Angle) add(b Angle) Angle {
 	return add(a, b)
 }
 
+// subtract subtract an angle from this angle
 pub fn (a Angle) subtract(b Angle) Angle {
 	return subtract(a, b)
 }
--- point_xy.v
+++ point_xy.v
@@ -16,6 +16,7 @@ pub:
 	y f64
 }
 
+// point_xy create a new PointXY with the given X and Y values
 pub fn point_xy(x f64, y f64) PointXY {
 	return PointXY{
 		x: x
@@ -23,22 +24,30 @@ pub fn point_xy(x f64, y f64) PointXY {
 	}
 }
 
+// add add two PointXY instances together
 pub fn add(a PointXY, b PointXY) PointXY {
 	return point_xy(a.x + b.x, a.y + b.y)
 }
 
+// add add a PointXY to this PointXY
 pub fn (a PointXY) add(b PointXY) PointXY {
 	return add(a, b)
 }
 
+// subtract subtract PointXY a from PointXY b
 pub fn subtract(a PointXY, b PointXY) PointXY {
 	return point_xy(b.x - a.x, b.y - a.y)
 }
 
+// subtract subtract a PointXY from this PointXY
 pub fn (a PointXY) subtract(b PointXY) PointXY {
 	return subtract(a, b)
 }
 
+// in_direction create a new PointXY a given distance away from the base
+// point, in the provided direction. a direction of 0 degrees will only
+// increase the new point's X value, while a direction of 90 degrees will
+// only increase the new point's Y value.
 pub fn in_direction(base PointXY, distance f64, angle angle.Angle) PointXY {
 	return PointXY{
 		x: base.x + (distance * angle.cos())
@@ -46,10 +55,15 @@ pub fn in_direction(base PointXY, distance f64, angle angle.Angle) PointXY {
 	}
 }
 
+// in_direction create a new PointXY a given distance away from this point,
+// in the provided direction. a direction of 0 degrees will only
+// increase the new point's X value, while a direction of 90 degrees will
+// only increase the new point's Y value.
 pub fn (base PointXY) in_direction(distance f64, angle angle.Angle) PointXY {
 	return in_direction(base, distance, angle)
 }
 
+// distance get the distance between two points
 pub fn distance(a PointXY, b PointXY) f64 {
 	dx := b.x - a.x
 	dy := b.y - a.y
@@ -60,26 +74,32 @@ pub fn distance(a PointXY, b PointXY) f64 {
 	return math.sqrt(x + y)
 }
 
+// distance get how far from this point another point is
 pub fn (a PointXY) distance(b PointXY) f64 {
 	return distance(a, b)
 }
 
+// distance_x get the X distance between two points
 pub fn distance_x(a PointXY, b PointXY) f64 {
 	return b.x - a.x
 }
 
+// distance_x get the X distance between two points
 pub fn (a PointXY) distance_x(b PointXY) f64 {
 	return distance_x(a, b)
 }
 
+// distance_y get the Y distance between two points
 pub fn distance_y(a PointXY, b PointXY) f64 {
 	return b.y - a.y
 }
 
+// distance_y get the Y distance between two points
 pub fn (a PointXY) distance_y(b PointXY) f64 {
 	return distance_y(a, b)
 }
 
+// angle_to get the angle FROM point a TO point b
 pub fn angle_to(a PointXY, b PointXY) angle.Angle {
 	y := distance_y(a, b)
 	x := distance_x(a, b)
@@ -87,30 +107,41 @@ pub fn angle_to(a PointXY, b PointXY) angle.Angle {
 	return angle.atan2(y, x)
 }
 
+// angle_to get the angle FROM this point TO the provided point
 pub fn (a PointXY) angle_to(b PointXY) angle.Angle {
 	return angle_to(a, b)
 }
 
+// angle_from get the angle TO point a FROM point b
 pub fn angle_from(a PointXY, b PointXY) angle.Angle {
 	return angle_to(b, a)
 }
 
+// angle_from get the angle TO this point FROM the provided point
 pub fn (a PointXY) angle_from(b PointXY) angle.Angle {
 	return angle_from(a, b)
 }
 
+// rotate rotate a point around a center point by a given angle
 pub fn rotate(point PointXY, center PointXY, angle angle.Angle) PointXY {
 	return in_direction(center, distance(center, point), angle_to(center, point).add(angle))
 }
 
+// rotate rotate a point around a center point by a given angle
 pub fn (point PointXY) rotate(center PointXY, angle angle.Angle) PointXY {
 	return rotate(point, center, angle)
 }
 
+// is_near check if two points are near. if the distance between the two points
+// is less than or equal to max_distance, the points are considered "near." if
+// the distance is greater than max_distance, the points are not near.
 pub fn is_near(a PointXY, b PointXY, max_distance f64) bool {
 	return distance(a, b) <= max_distance
 }
 
+// is_near check if two points are near. if the distance between the two points
+// is less than or equal to max_distance, the points are considered "near." if
+// the distance is greater than max_distance, the points are not near.
 pub fn (a PointXY) is_near(b PointXY, max_distance f64) bool {
 	return is_near(a, b, max_distance)
 }
